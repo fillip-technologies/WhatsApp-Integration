77754adad885bf780a99fa7432e1bc555479b781
Fillip Dev <developer@filliptechnologies.com>
2026-07-11 12:16:15 +0530

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->controller(AdminController::class)->middleware(['admin'])->group(function(){
 Route::get('/exportPaymentdata','exportPaymentdata')->name('exportPaymentdata');
+Route::get('profile/edit/{id}','AdminProfileEdit')->name('admin.edit');
 Route::get('/dashboard','adminDashboard')->name('admin.dashboard');
 Route::post('/logout','LogoutAdmin')->name('admin.logout');
 Route::get('/config/index','ConfigIndex')->name('config.index');
